af2825f2280c016402b0047c07ef276f1ea6f1e6
WestTexasAM <leeland_h@hotmail.com>
2015-04-20 23:50:09 -0500

    Project Menu

---
+++ app/controllers/projectMenu.js
@@ -0,0 +1,121 @@
+var args = arguments[0] || {};
+function doClick1() {
+		var win = Titanium.UI.createWindow({
+			title: 'PROJECT 1',
+		});
+		var back=Titanium.UI.createButton({
+			title:'Back',
+			width: Ti.UI.SIZE,
+			height: Ti.UI.SIZE,
+			bottom : 10,
+			left : 10
+		});
+			var info=Titanium.UI.createButton({
+			title:'Project 1 Information Goes Here',
+			width: Ti.UI.SIZE,
+			height: Ti.UI.SIZE
+		});
+		win.add(info);
+		win.add(back);
+		win.open();
+};
+function doClick2() {
+		var win = Titanium.UI.createWindow({
+			title: 'PROJECT 2',
+		});
+		var back=Titanium.UI.createButton({
+			title:'Back',
+			width: Ti.UI.SIZE,
+			height: Ti.UI.SIZE,
+			bottom : 10,
+			left : 10
+		});
+			var info=Titanium.UI.createButton({
+			title:'Project 2 Information Goes Here',
+			width: Ti.UI.SIZE,
+			height: Ti.UI.SIZE
+		});
+		win.add(info);
+		win.add(back);
+		win.open();
+};
+function doClick3() {
+		var win = Titanium.UI.createWindow({
+			title: 'PROJECT 3',
+		});
+		var back=Titanium.UI.createButton({
+			title:'Back',
+			width: Ti.UI.SIZE,
+			height: Ti.UI.SIZE,
+			bottom : 10,
+			left : 10
+		});
+			var info=Titanium.UI.createButton({
+			title:'Project 3 Information Goes Here',
+			width: Ti.UI.SIZE,
+			height: Ti.UI.SIZE
+		});
+		win.add(info);
+		win.add(back);
+		win.open();
+};
+function doClick4() {
+		var win = Titanium.UI.createWindow({
+			title: 'PROJECT 4',
+		});
+		var back=Titanium.UI.createButton({
+			title:'Back',
+			width: Ti.UI.SIZE,
+			height: Ti.UI.SIZE,
+			bottom : 10,
+			left : 10
+		});
+			var info=Titanium.UI.createButton({
+			title:'Project 4 Information Goes Here',
+			width: Ti.UI.SIZE,
+			height: Ti.UI.SIZE
+		});
+		win.add(info);
+		win.add(back);
+		win.open();
+};
+function doClick5() {
+		var win = Titanium.UI.createWindow({
+			title: 'PROJECT 5',
+		});
+		var back=Titanium.UI.createButton({
+			title:'Back',
+			width: Ti.UI.SIZE,
+			height: Ti.UI.SIZE,
+			bottom : 10,
+			left : 10
+		});
+			var info=Titanium.UI.createButton({
+			title:'Project 5 Information Goes Here',
+			width: Ti.UI.SIZE,
+			height: Ti.UI.SIZE
+		});
+		win.add(info);
+		win.add(back);
+		win.open();
+};
+function doClick6() {
+		var win = Titanium.UI.createWindow({
+			title: 'PROJECT 6',
+		});
+		var back=Titanium.UI.createButton({
+			title:'Back',
+			width: Ti.UI.SIZE,
+			height: Ti.UI.SIZE,
+			bottom : 10,
+			left : 10
+		});
+			var info=Titanium.UI.createButton({
+			title:'Project 6 Information Goes Here',
+			width: Ti.UI.SIZE,
+			height: Ti.UI.SIZE
+		});
+		win.add(info);
+		win.add(back);
+		win.open();
+};
